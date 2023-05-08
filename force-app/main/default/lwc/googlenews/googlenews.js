8c15fdf9d5f5d043caa881e47101edf9e87c673d
iqbalaust <iqbalbdrocky@gmail.com>
2023-05-08 23:44:46 +0600

    jh

---
+++ force-app/main/default/lwc/googlenews/googlenews.js
@@ -0,0 +1,41 @@
+import { LightningElement,track, api,wire } from 'lwc';
+import getArticleList from '@salesforce/apex/GoogleNewsService.getCallout';
+
+export default class Googlenews extends LightningElement {
+
+    @track dataList = [];
+    @track value = 'business';
+    
+    get options() {
+        return [
+            { label: 'Business', value: 'business' },
+            { label: 'Entertainment', value: 'entertainment' },
+            { label: 'Health', value: 'health' },
+            { label: 'Science', value: 'science' },
+            { label: 'Sports', value: 'sports' },
+            { label: 'Technology', value: 'technology' },
+           
+        ];
+    }
+
+    handleChange(event){
+        this.value = event.detail.value;
+        this. getArticleList();
+    }
+
+    connectedCallback(){
+
+        this. getArticleList();
+    }
+
+    getArticleList(){
+        getArticleList({category: this.value})
+        .then(result=>{
+            this.dataList = result;
+        })
+        .catch(error=>{
+            console.log('error' +error);
+        })
+    }
+
+}
